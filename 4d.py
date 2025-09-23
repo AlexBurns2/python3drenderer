b539838f17bffd059878d30846a99630ec1ad2e0
Alex Burns <Alex.burns6@education.nsw.gov.au>
2025-09-24 08:35:35 +1000

    4d rotation

--- 4d.py
+++ 4d.py
@@ -6,7 +6,7 @@ from itertools import product
 
 WIDTH, HEIGHT = 500, 500
 zoom = 200
-angle_x, angle_y, angle_z, angle_w = 0, 0, 0, 0
+azw, ayw, ayz, axw, axz, axy, = 0, 0, 0, 0, 0, 0
 
 vertices = np.array(list(product([-1, 1], repeat=4)))
 
@@ -16,6 +16,11 @@ for i, p1 in enumerate(vertices):
         if i<j and np.sum(np.abs(p2 - p1)) == 2:
             edges.append((i,j))
 edges = np.array(edges)
+
+angles = {
+    "xy": 0.0, "xz": 0.0, "xw": 0.0,
+    "yz": 0.0, "yw": 0.0, "zw": 0.0
+}
 
 print(edges)
 
@@ -29,19 +34,27 @@ def project_point(x, y, z, w, width, height):
     y_proj = -y * factor2 * zoom/200 + height/2
     return (x_proj, y_proj)
 
-def rotate_point(x, y, z, w, ax, ay, az, aw):
-    if aw !=0:
-        y = math.cos(aw) - math.sin(aw)
-        x = math.sin(aw) + math.cos(aw)
-        z = z
-        w = w
-    elif ax !=0:
-        y = math.cos(ax) - math.sin(ax)
-        x = x
-        z = z
-        w = math.sin(ax) + math.cos(ax)
-
-    return x, y, z, w
+def rotate_point(x, y, z, w, angles):
+    cos, sin = math.cos, math.sin
+    # XY-plane
+    c,s = cos(angles.get("xy",0)), sin(angles.get("xy",0))
+    x,y = c*x - s*y, s*x + c*y
+    # XZ-plane
+    c,s = cos(angles.get("xz",0)), sin(angles.get("xz",0))
+    x,z = c*x - s*z, s*x + c*z
+    # XW-plane
+    c,s = cos(angles.get("xw",0)), sin(angles.get("xw",0))
+    x,w = c*x - s*w, s*x + c*w
+    # YZ-plane
+    c,s = cos(angles.get("yz",0)), sin(angles.get("yz",0))
+    y,z = c*y - s*z, s*y + c*z
+    # YW-plane
+    c,s = cos(angles.get("yw",0)), sin(angles.get("yw",0))
+    y,w = c*y - s*w, s*y + c*w
+    # ZW-plane
+    c,s = cos(angles.get("zw",0)), sin(angles.get("zw",0))
+    z,w = c*z - s*w, s*z + c*w
+    return x,y,z,w
 
 def draw_cube():
     canvas.delete("all")
@@ -49,7 +62,7 @@ def draw_cube():
     projected = []
     for v in vertices:
         #x, y, z, w = v[0], v[1], v[2], v[3]
-        x, y, z, w = rotate_point(v[0], v[1], v[2], v[3], angle_x, angle_y, angle_z, angle_w)
+        x, y, z, w = rotate_point(*v, angles)
         projected.append(project_point(x, y, z, w, width, height))
     for e in edges:
         x1, y1 = projected[e[0]]
@@ -62,15 +75,15 @@ def update():
     root.after(20, update)
 
 def on_key(event):
-    global angle_x, angle_y, angle_w, angle_z, zoom
+    global azw, ayz, zoom
     if event.keysym == "Left":
-        angle_w -= math.pi/10
+        angles["zw"] -= math.pi/10
     elif event.keysym == "Right":
-        angle_w += math.pi/10
+        angles["zw"] += math.pi/10
     elif event.keysym == "Up":
-        angle_x -= math.pi/10
+        angles["yz"] -= math.pi/10
     elif event.keysym == "Down":
-        angle_x += math.pi/10
+        angles["yz"] += math.pi/10
     elif event.keysym == "plus":
         zoom += 10
     elif event.keysym == "minus":
