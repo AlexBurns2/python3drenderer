7c9c0611a7cd9d494c2808b5cf93f7b76c230e7c
tony <drtonyburns@hotmail.com>
2025-09-25 14:29:09 +1000

    quaternion

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ colors = [
 angle_x, angle_y = 0.5, 0.5
 zoom = 200
 last_mouse = None
+qw, qx, qy, wz = 0,0,0,0
 
 def rotate_point(x,y,z,ax,ay):
     cosx,sinx = math.cos(ax), math.sin(ax)
@@ -40,9 +41,74 @@ def rotate_point(x,y,z,ax,ay):
     x,z = x*cosy + z*siny, -x*siny + z*cosy #y axis rotation matrix
     return x,y,z
 
+class Quaternion:
+    def __init__(self, w, x, y, z):
+        self.w = w
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def multiply(a, b):
+        w = a.w*b.w - a.x*b.x - a.y*b.y - a.z*b.z
+        x = a.w*b.x + a.x*b.w + a.y*b.z - a.z*b.y
+        y = a.w*b.y + a.x*b.z + a.y*b.w - a.z*b.x
+        z = a.w*b.z + a.x*b.y + a.y*b.x - a.z*b.w
+        return w,x,y,z
+    
+    def conjugate(self):
+        return self.w, -self.x, -self.y, -self.z
+
+def quaternion_rotation(x,y,z,ax, ay, az, t):
+    v = Quaternion(0, x, y, z)
+
+    q = Quaternion(
+        math.cos(t/2),
+        ax * math.sin(t/2),
+        ay * math.sin(t/2),
+        az * math.sin(t/2)
+    )
+    qprime = q.conjugate
+    out = q.multiply(v).multiply(qprime)
+    return out.x, out.y, out.z
+
+
+
+    '''
+    cx = math.cos(ax/2)
+    sx = math.sin(ax/2)
+    cy = math.cos(ay/2)
+    sy = math.sin(ay/2)
+    cz = math.cos(az/2)
+    sz = math.sin(az/2)
+
+    q = {
+        "w": 0.0, "x": 0.0, "y": 0.0, "z": 0.0
+        }
+
+    q["w"] = cx*cy*cz + sx*sy*sz
+    q["x"] = sx*cy*cz - cx*sy*sz
+    q["y"] = cx*sy*cz + sx*cy*sz
+    q["z"] = cx*cy*sz - sx*sy*cz
+
+    qprime = {
+        "w": q["w"], "x": -q["x"], "y": -q["y"], "z": -q["z"]
+        }
+    
+    out = {
+        "x": 0.0, "y": 0.0, "z": 0.0
+        }
+    
+    #hamilton product
+    for key in q:
+        for keyprime in qprime:
+            out["x"] += q[key] * qprime[keyprime] * x
+            out["y"] += q[key] * qprime[keyprime] * y
+            out["z"] += q[key] * qprime[keyprime] * z
+    return out["x"], out["y"], out["z"] '''
+
 def project_point(x,y,z):
-    fov,dist = 256,4 #abritrary fov
-    factor = fov/(z+ dist)
+    fov,dist = 256, 4 #abritrary fov
+    factor = fov/(z + dist)
     return int(x * factor * zoom / 100 + WIDTH/2), int(-y * factor * zoom / 100 + HEIGHT/2), z #scaled by zoom and perspective distort
 
 def draw_scene():
@@ -50,7 +116,8 @@ def draw_scene():
     zbuf = np.full((HEIGHT,WIDTH), np.inf, dtype=np.float32) #z buffer definition
     img = np.zeros((HEIGHT,WIDTH,3), dtype=np.uint8) #all black
 
-    transformed = [rotate_point(*v,angle_x,angle_y) for v in vertices]
+    transformed = [quaternion_rotation(*v,angle_x,angle_y,0) for v in vertices]
+    #transformed = [rotate_point(*v,angle_x,angle_y) for v in vertices]
     projected = [project_point(*v) for v in transformed]
 
     for tri,col in zip(tris,colors):
@@ -91,7 +158,7 @@ def on_mouse_drag(event):
     if last_mouse is not None:
         dx = event.x - last_mouse[0]
         dy = event.y - last_mouse[1]
-        angle_x -= dy * 0.005
+        angle_x += dy * 0.005
         angle_y -= dx * 0.005
         last_mouse = (event.x, event.y)
 
